0e941cdff03d592e23bc169e903aa09277a4cb7f
Sasha <alxblag@ya.ru>
2019-03-15 22:46:08 +0200

    module8-task2

--- templates/add.php
+++ templates/add.php
@@ -25,13 +25,12 @@
 
                 <?php if ($is_auth): ?>
 
-                    <?php if (is_null($user_avatar)): ?>
-
-                    <?php else: ?>
+                    <?php if (!empty($user_avatar)): ?>
                         <div class="user-menu__image">
                             <img src="<?=$user_avatar; ?>" width="200" height="200" alt="Пользователь">
                         </div>
                     <?php endif; ?>
+
 
                     <div class="user-menu__logged">
                         <p> <?=screening_txt($user_name); ?></p>
